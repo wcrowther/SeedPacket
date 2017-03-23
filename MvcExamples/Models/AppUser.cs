df00890c1d808eaa761c559408aaa1ce88c71fd0
wcrowther <wcrowther@0f34a797-f61f-454c-90bd-cc52d0c5a539>
2017-03-23 09:12:03 +0000

    Added models. Changed name of Multi.. to Dual...

---
+++ MvcExamples/Models/AppUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcExamples.Models
+{
+    public class AppUser
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public List<Item> Items { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
