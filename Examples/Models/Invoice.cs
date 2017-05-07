dd673b749659055cf9bfbbdcbec71a91719f4f3f
wcrowther <wcrowther@0f34a797-f61f-454c-90bd-cc52d0c5a539>
2017-05-07 20:39:53 +0000

    Adding Examples project.

---
+++ Examples/Models/Invoice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Website.Models
+{
+    public class Invoice
+    {
+        public int InvoiceId { get; set; }
+        public int AccountId { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public DateTime Created { get; set; }
+        public virtual List<InvoiceItem> InvoiceItems { get; set; } = new List<InvoiceItem>();
+    }
+}
---
+++ Examples/Scripts/_references.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="jquery-1.10.2.js" />
+/// <autosync enabled="true" />
+/// <reference path="../scriptapp/seedexamples.js" />
+/// <reference path="bootstrap.min.js" />
+/// <reference path="modernizr-2.6.2.js" />
+/// <reference path="respond.js" />
