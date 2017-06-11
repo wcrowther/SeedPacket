f398a138a4e5a8c406a568f6b342c3cd262f6f3b
wcrowther <wcrowther@0f34a797-f61f-454c-90bd-cc52d0c5a539>
2017-06-11 02:26:19 +0000

    Added more progress on Examples and some missing files.

---
+++ WebSite/Models/Song.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Website.Models
+{
+    public class Song
+    {
+        public int Ranking { get; set; }
+        public string SongTitle { get; set; }
+        public string AlbumName { get; set; }
+        public string Artist { get; set; }
+        public DateTime Released { get; set; }
+    }
+}
